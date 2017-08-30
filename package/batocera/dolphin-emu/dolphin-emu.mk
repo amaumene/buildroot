0a9fa8a6465a4b82d325d65efc2fc207d8bdbe02
amaumene <amaumene@redhat.com>
2017-08-30 14:35:31 +0800

    add plugixml as dep because I want to use the system one

--- package/batocera/dolphin-emu/dolphin-emu.mk
+++ package/batocera/dolphin-emu/dolphin-emu.mk
@@ -7,7 +7,7 @@
 # 5.0 + pad name bug fixed
 DOLPHIN_EMU_VERSION = 1705d15d6f3fe1c2f760a4b6bc70542d94b9e9f2
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
-DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils wxwidgets hidapi
+DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils wxwidgets hidapi pugixml
 
 # bluez is disable otherwise a compilation error appears
 #DOLPHIN_EMU_CONF_OPTS = -DOPENGL_INCLUDE_DIR=$(STAGING_DIR)/usr/include
